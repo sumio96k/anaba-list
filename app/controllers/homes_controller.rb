b6e1fbb8bd0dc9d5aff32955c4a62f2c53f2e7c1
sumio96k <106654738+sumio96k@users.noreply.github.com>
2022-08-20 07:52:56 +0000

    update

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,5 +1,8 @@
 class HomesController < ApplicationController
   def top
+    #コメントの多い順＋評価の高い順
+    # post_comments_lank = Post.find(PostComment.group(:post_id).order('count(post_id) desc').pluck(:post_commnt)
+    #投稿数の多い順で並び替えしたい
     @prefectures = Prefecture.where(name: ['東京都', '大阪府', '福岡県'])
   end
 
