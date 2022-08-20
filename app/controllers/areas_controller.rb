cf9dd6ec5b7ebd851fa2c2dca0fcb8f8bf4e7af4
sumio96k <106654738+sumio96k@users.noreply.github.com>
2022-08-20 09:56:41 +0000

    update

--- app/controllers/areas_controller.rb
+++ app/controllers/areas_controller.rb
@@ -5,7 +5,7 @@ class AreasController < ApplicationController
 
   def show
     @categories = Category.all
-    @area = Area.find(params[:id]).limit(12).order(id: "DESC")
-    @posts = @area.posts
+    @area = Area.find(params[:id])
+    @posts = @area.posts.limit(12).order(id: "DESC")
   end
 end
