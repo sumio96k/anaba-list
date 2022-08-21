5f7ff572a2375e61e59646060052dcc7570eaf37
sumio96k <106654738+sumio96k@users.noreply.github.com>
2022-08-21 05:37:59 +0000

    footer

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,8 +2,8 @@ class HomesController < ApplicationController
   def top
     #コメントの多い順＋評価の高い順
     #投稿数の多い順で並び替えしたい
-    @post_lanks = Post.find(PostComment.group(:post_id).order('count(post_id) desc').pluck(:post_id))
-    
+    @post_lanks = Post.find(PostComment.group(:post_id).order('count(post_id)').pluck(:post_id))
+
     @prefectures = Prefecture.where(name: ['東京都', '大阪府', '福岡県'])
   end
 
