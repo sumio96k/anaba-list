c2cc39a7d8756eb75ec767d1ff5348cd85a3ba8f
sumio96k <106654738+sumio96k@users.noreply.github.com>
2022-08-22 02:44:24 +0000

    update

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -4,8 +4,8 @@ class HomesController < ApplicationController
     post_comment_lanks = Post.find(PostComment.group(:post_id).limit(10).order('count(post_id) desc').pluck(:post_id))
     #さらにrateの高い順で並び替える
     @post_rate_lanks = post_comment_lanks.sort{|a,b| b[:rate].to_i<=>a[:rate].to_i}
-
-    @prefectures = Prefecture.where(name: ['東京都', '大阪府', '福岡県'])
+    #投稿数の多いエリアを取得してそこから都道府県を取得する
+    @prefectures = Prefecture.find(Area.find(post_comment_lanks.pluck(:area_id)).pluck(:prefecture_id))
   end
 
   def about
